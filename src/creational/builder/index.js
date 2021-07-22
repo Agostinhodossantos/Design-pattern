09e75c69b3695a7448582d246ca99b3028588800
= <=>
2021-07-22 06:36:03 -0700

    add index

---
+++ src/creational/builder/index.js
@@ -0,0 +1,10 @@
+import { Beans, Meat, Rice } from "./classes/meals";
+import { MealBox } from "./classes/meal";
+
+const rice = new Rice("arroz", 5);
+const beans = new Beans("Feijao", 10);
+const meat = new Meat("Carne", 20);
+const mealBox = new MealBox();
+mealBox.add(rice, beans, meat);
+console.log(mealBox);
+console.log(mealBox.getPrice());

2b8d0ff1c9237fdba529fe8c2f47dba3e4e994b0
= <=>
2021-07-23 00:19:54 -0700

    add main-dish-builder

---
+++ src/creational/builder/classes/main-dish-builder.ts
@@ -0,0 +1,34 @@
+import { MealBuilderProtocol } from "../interfaces/meal-builder-protocol";
+import { MealBox } from "./meal-box";
+import { Beans, Beverage, Dessert, Meat, Rice } from "./meals";
+
+export class MainDishBuilder implements MealBuilderProtocol {
+  private _meal: MealBox = new MealBox();
+
+  reset(): this {
+    this._meal = new MealBox();
+    return this;
+  }
+
+  makeMeal(): this {
+    const rice = new Rice("Arroz", 10);
+    const beans = new Beans("Feijao", 10);
+    const meat = new Meat("Carne", 20);
+    this._meal.add(rice, beans, meat);
+    return this;
+  }
+  makeBeverage(): this {
+    const beverage = new Beverage("Bebida", 40);
+    this._meal.add(beverage);
+    return this;
+  }
+  makeDesert(): this {
+    const desert = new Dessert("sobremesa", 10);
+    this._meal.add(desert);
+    return this;
+  }
+
+  getMeal(): MealBox {
+    return this._meal;
+  }
+}
